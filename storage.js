105298e88e1629bd8639ab3150ad85e9d90f337a
Camila B <Lcamila.bustos@yahoo.com>
2021-11-16 14:25:36 -0600

    Added written entry feature

--- storage.js
+++ storage.js
@@ -2,10 +2,10 @@ let _storage = window.localStorage;
 let _pageId = this.location.pathname.split("/").pop();
 
 let _state = {
-  inputs: []
+  inputs: [],
 };
 
-(function initStorage(){
+(function initStorage() {
   loadState();
 
   loadViewInputs();
@@ -14,89 +14,89 @@ let _state = {
   initViewInputs();
 })();
 
-function loadViewInputs(){
-  
-  for (i of _state.inputs){
+function loadViewInputs() {
+  for (i of _state.inputs) {
     if (i.pageId != _pageId) continue;
 
     let elem = document.getElementById(i.elemId);
-    if (i.type==="radio") updateRadioGroup(i.elemId, i.text); // elemId of radio is the name
-    if (i.type==="checkbox") elem.checked = i.value;
-    if (i.type==="textarea") elem.value = i.value;
-    if (i.type==="text") elem.value = i.value;
+    if (i.type === "radio") updateRadioGroup(i.elemId, i.text); // elemId of radio is the name
+    if (i.type === "checkbox") elem.checked = i.value;
+    if (i.type === "textarea") elem.value = i.value;
+    if (i.type === "text") elem.value = i.value;
   }
 }
 
-function updateRadioGroup(radioName, value){
-  $(`input[type=radio][name=${radioName}]`).each(
-      (i,e)=>{
-        e.checked = $(`label[for=${e.id}]`).text()===value});
+function updateRadioGroup(radioName, value) {
+  $(`input[type=radio][name=${radioName}]`).each((i, e) => {
+    e.checked = $(`label[for=${e.id}]`).text() === value;
+  });
 }
 
-function initViewInputs(){
+function initViewInputs() {
   $(".view textarea").change(changeTextAreaInput);
   $(".view input[type=text]").change(changeTextInput);
   $(".view input[type=radio]").change(changeRadioInput);
   $(".view input[type=checkbox]").change(changeCheckboxInput);
 }
 
-function changeTextAreaInput(e){
+function changeTextAreaInput(e) {
   let data = {
     elemId: e.target.id,
     viewId: $(e.target).closest(".view")[0].id,
     value: e.target.value,
-    type: "textarea"
-  }
-
-  storeInput(data);
-}
-
-function changeTextInput(e){
-  let data = {
-    elemId: e.target.id,
-    viewId: $(e.target).closest(".view")[0].id,
-    value: e.target.value,
-    type: "text"
+    type: "textarea",
   };
 
   storeInput(data);
 }
 
-function changeRadioInput(e){
+function changeTextInput(e) {
+  let data = {
+    elemId: e.target.id,
+    viewId: $(e.target).closest(".view")[0].id,
+    value: e.target.value,
+    type: "text",
+  };
+
+  storeInput(data);
+}
+
+function changeRadioInput(e) {
   if (!e.target.checked) return;
 
   let data = {
-    elemId:  e.target.getAttribute("name"),
+    elemId: e.target.getAttribute("name"),
     radioId: e.target.id,
-    viewId:  $(e.target).closest(".view")[0].id,
-    text:  $(`label[for=${e.target.id}]`).text(),
-    type: "radio"
+    viewId: $(e.target).closest(".view")[0].id,
+    text: $(`label[for=${e.target.id}]`).text(),
+    type: "radio",
   };
 
   storeInput(data);
-
 }
 
-function changeCheckboxInput(e){
-
+function changeCheckboxInput(e) {
   let data = {
     elemId: e.target.id,
     viewId: $(e.target).closest(".view")[0].id,
-    text:  $(`label[for=${e.target.id}]`).text(),
+    text: $(`label[for=${e.target.id}]`).text(),
     value: e.target.checked,
-    type: "checkbox"
+    type: "checkbox",
   };
 
   storeInput(data);
-
 }
 
-function storeInput(data){
+function storeInput(data) {
   let pageId = _pageId;
-  let input = {pageId, ...data};
+  let input = { pageId, ...data };
 
   let oldIndex = _state.inputs.findIndex(
-    i=>i.pageId==input.pageId && i.viewId==input.viewId && i.elemId==input.elemId);
+    (i) =>
+      i.pageId == input.pageId &&
+      i.viewId == input.viewId &&
+      i.elemId == input.elemId
+  );
 
   if (oldIndex >= 0) _state.inputs[oldIndex] = input;
   else _state.inputs.push(input);
@@ -104,12 +104,12 @@ function storeInput(data){
   storeState();
 }
 
-function storeState(){
-  let json = JSON.stringify({data:_state});
-  _storage.setItem("myndfully",json);
+function storeState() {
+  let json = JSON.stringify({ data: _state });
+  _storage.setItem("myndfully", json);
 }
 
-function loadState(){
+function loadState() {
   let json = _storage.getItem("myndfully");
   if (json != null) _state = JSON.parse(json).data;
 }

d6075d18ddf68691c75c15492cb9d7f7dfa9a62e
Scott Jodoin <scottjodoin@gmail.com>
2021-11-19 15:29:51 -0600

    add icons to chart

--- view-entries.js
+++ view-entries.js
@@ -4,15 +4,35 @@ var entryModal = new bootstrap.Modal(
 ); // Returns a Bootstrap modal instance
 
 const ctx = document.getElementById('chart');
+const annoCanvas = document.getElementById('annotation');
+const annoCtx = annoCanvas.getContext('2d');
 
 function pointColor(context){
     var index = context.dataIndex;
-    var value = context.dataset.data[index].y;
-    return value == 0 ? '#808080' : // draw 0 in grey 
-        value > 0 ? '#5daa68ff' : // positive in green 
-            '#cc5500'; // negative in red
+   
+    return data.weeks[week].dataPoints[index].mood.color;
+    // var value = context.dataset.data[index].y;
+    // return value == 0 ? '#808080' : // draw 0 in grey 
+    //     value > 0 ? '#5daa68ff' : // positive in green 
+    //         '#cc5500'; // negative in red
+}
+let emotionIcons = {};
+
+for (i =0 ; i < 25; i++){
+  let iconName = "e"+pad(i+1,2);
+  let img = new Image(100, 100);
+  img.src = `./img/${iconName}.svg`;
+
+  emotionIcons[iconName] = img;
 }
 
+function pad(num, size) {
+  var s = "000000000" + num;
+  return s.substr(s.length-size);
+}
+
+const POINT_RADIUS = 20;
+const POINT_HOVER_RADIUS = 25;
 let week = 0;
 const chart = new Chart(ctx, {
   type: 'line',
@@ -26,10 +46,11 @@ const chart = new Chart(ctx, {
       
       borderWidth: 3,
       borderColor: "#808080",
-      pointRadius: 10,
-      pointHoverRadius: 15,
+      pointRadius: POINT_RADIUS,
+      pointHoverRadius: POINT_HOVER_RADIUS,
       pointBackgroundColor: pointColor,
-      pointBorderColor: pointColor
+      pointBorderColor: pointColor, 
+      pointStyle: emotionIcons[0]
     }]
   },
 
@@ -50,10 +71,35 @@ const chart = new Chart(ctx, {
       }
     },
   },
-  plugins: [{
-  }]
-  
+  plugins: [{afterDraw: drawOnPoints}]  
 });
+
+function updateIcons (chart){
+  //annoCtx.drawImage(emotionIcons[0], 0, 0);
+
+  // console.log(chart.config);
+  // chart.options.elements.point.pointStyle = emotionIcons["e01"];
+  // chart.config._config.data.datasets[0].pointStyle = emotionIcons["e01"];
+  // chart.options.elements.point.pointStyle = emotionIcons["e01"];
+  // chart.config._config.data.datasets[0].data[0].pointStyle = emotionIcons["e01"];
+  // chart.config.data.datasets[0]._meta[0].data[7]._model.pointStyle = emotionIcons["e01"];
+}
+function drawOnPoints(chart, args, options){
+  annoCanvas.width = chart.width;
+  annoCanvas.height = chart.height;
+
+  let chartData = chart._metasets[0].data;
+  
+  for (i = 0; i < chartData.length; i++){
+    let p = chartData[i];
+    let stretchFactor = 2;
+    let iconName = data.weeks[week].dataPoints[i].mood.icon;
+    console.log(iconName);
+    let img = emotionIcons[iconName];
+    if (!img) continue;
+    annoCtx.drawImage(img, p.x - POINT_RADIUS*stretchFactor/2, p.y - POINT_RADIUS*stretchFactor/2, POINT_RADIUS * stretchFactor, POINT_RADIUS * stretchFactor);
+  }
+}
 
 // clicking on the canvas
 $(ctx).on("click", canvasClicked);
@@ -77,8 +123,11 @@ function canvasClicked(event) {
     let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
 
     $("#entry-modal-title").text(`${mo} ${da}, ${ye}`);
-    $("#entry-date").val(dateInfo.date);
-    $("#entry-mood").val(dateInfo.mood);
+    $("#entry-date").text(dateInfo.date);
+    $("#entry-mood").attr("src", `img/${dateInfo.mood.icon}.svg`);
+    $("#entry-mood-wrapper").css("background-color", dateInfo.mood.color);
+
+    $("#entry-mood-text").text(dateInfo.mood.name);
     $("#entry-notes").val(dateInfo.entry);
 
     pointClicked(label,value);
@@ -116,5 +165,6 @@ function updateChart() {
   chart.data.datasets[0].data = 
     weekData.dataPoints.map(e=>{return {x:e.x, y:e.y}});
   $("#week-title").text(weekData.weekTitle);
+
   chart.update();
 }

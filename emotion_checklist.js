e030dec9c1ca24ee7936c6c6771c2d6038dd0cdf
Camila B <Lcamila.bustos@yahoo.com>
2021-11-29 15:20:08 -0600

    Made emotion view bigger

--- emotion_checklist.js
+++ emotion_checklist.js
@@ -3,7 +3,7 @@
   let emotionsByCat = {};
 
   // group emotions by color
-  for (emotion of _emotion_data){
+  for (emotion of _emotion_data) {
     let cat = emotion.category;
     if (emotionsByCat[cat] == undefined) {
       emotionsByCat[cat] = [];
@@ -12,24 +12,23 @@
   }
 
   // create html for each color
-  html += "<div class='container d-flex flex-wrap' id='emotion-checklist-container'>";
-  for (cat in emotionsByCat){
+  html +=
+    "<div class='container d-flex flex-wrap' id='emotion-checklist-container'>";
+  for (cat in emotionsByCat) {
+    html += "<div class=row row-cols-7>";
 
-    html += "<span class=row row-cols-7>";
-
-    for (emotion of emotionsByCat[cat]){
-      
+    for (emotion of emotionsByCat[cat]) {
       let checkId = `emotion-check-${emotion.name.split(" ").join("-")}`;
-      
+
       html += `<div class="col me-1" id='emotion-checklist-item'>
         <input type="checkbox" id=${checkId} name=${checkId} class="btn-check">
         <label  style="background-color:${emotion.color};"for="${checkId}" class="my-3 btn btn-primary-outline btn-square">
           <img src="./img/${emotion.icon}.svg" width=40 height=40>
           <span class="text-center">${emotion.name}</span>
         </label>
-      </div>`;  
+      </div>`;
     }
-    html+= "</span>";
+    html += "</div>";
   }
   html += "</div>";
   $("emotion-checklist").html(html);

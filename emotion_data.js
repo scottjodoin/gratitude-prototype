8413dd3359c4abef3c7a384c6629a1a0fd6d74d5
Child-Of-Nyx <Child-Of-Nyx@users.noreply.github.com>
2021-11-29 13:49:40 -0600

    Add Emotion data into main checkin process

--- emotion_data.js
+++ emotion_data.js
@@ -1,19 +1,7 @@
-const _emotion_data = [
-  { "group": "upset", "name": "scared", "color": "royalblue","category":"cat1", "score": -1, "icon": "e19" },
-  { "group": "upset", "name": "threatened", "color": "royalblue","category":"cat1", "score": -1, "icon": "e25" },
-  { "group": "upset", "name": "sad", "color": "grey","category":"cat1", "score": -1, "icon": "e01" },
-  { "group": "upset", "name": "lonely", "color": "grey","category":"cat1", "score": -1, "icon": "e03" },
-  { "group": "upset", "name": "hopeless", "color": "grey","category":"cat1", "score": -1, "icon": "e06" },
-  { "group": "upset", "name": "embarrassed", "color": "grey","category":"cat1", "score": -1, "icon": "e20" },
-  { "group": "upset", "name": "angry", "color": "tomato","category":"cat1", "score": -1, "icon": "e17" },
-  { "group": "upset", "name": "jealous", "color": "tomato","category":"cat1", "score": -1, "icon": "e11" },
-  { "group": "upset", "name": "frustrated", "color": "tomato","category":"cat1", "score": -1, "icon": "e17" },
-  { "group": "upset", "name": "disgust", "color": "tomato","category":"cat1", "score": -1, "icon": "e19" },
-  { "group": "upset", "name": "confused", "color": "purple","category":"cat1", "score": -1, "icon": "e25" },
-  { "group": "upset", "name": "stressed", "color": "purple","category":"cat1", "score": -1, "icon": "e19" },
-  { "group": "upset", "name": "tired", "color": "purple","category":"cat1", "score": -1, "icon": "e08" },
-  { "group": "upset", "name": "overwhelmed", "color": "purple","category":"cat1", "score": -1, "icon": "e19" },
+const _positive_colour = "orchid";
+const _negative_colour = "tomato";
 
+const _emotion_data = [
   { "group": "happy", "name": "confident", "color": "orchid","category":"cat2", "score": 1, "icon": "e15" },
   { "group": "happy", "name": "loving", "color": "orchid","category":"cat2", "score": 1, "icon": "e09" },
   { "group": "happy", "name": "brave", "color": "orchid","category":"cat2", "score": 1, "icon": "e04" },
@@ -21,11 +9,22 @@ const _emotion_data = [
   { "group": "happy", "name": "excited", "color": "seagreen","category":"cat2", "score": 1, "icon": "e05" },
   { "group": "happy", "name": "creative", "color": "seagreen","category":"cat2", "score": 1, "icon": "e14" },
   { "group": "happy", "name": "proud", "color": "gold","category":"cat2", "score": 1, "icon": "e09" },
-  { "group": "happy", "name": "respected", "color": "gold","category":"cat2", "score": 1, "icon": "e04" },
   { "group": "happy", "name": "optimistic", "color": "gold","category":"cat2", "score": 1, "icon": "e09" },
   { "group": "happy", "name": "thankful", "color": "gold","category":"cat2", "score": 1, "icon": "e09" },
   { "group": "happy", "name": "smart", "color": "gold","category":"cat2", "score": 1, "icon": "e05" },
   { "group": "happy", "name": "relieved", "color": "goldenrod","category":"cat2", "score": 1, "icon": "e08" },
-  { "group": "happy", "name": "appreciated", "color": "goldenrod","category":"cat2", "score": 1, "icon": "e09" },
   { "group": "happy", "name": "content", "color": "goldenrod","category":"cat2", "score": 1, "icon": "e04" },
+
+  { "group": "upset", "name": "scared", "color": "royalblue","category":"cat1", "score": -1, "icon": "e19" },
+  { "group": "upset", "name": "sad", "color": "grey","category":"cat1", "score": -1, "icon": "e01" },
+  { "group": "upset", "name": "lonely", "color": "grey","category":"cat1", "score": -1, "icon": "e03" },
+  { "group": "upset", "name": "hopeless", "color": "grey","category":"cat1", "score": -1, "icon": "e06" },
+  { "group": "upset", "name": "embarrassed", "color": "grey","category":"cat1", "score": -1, "icon": "e20" },
+  { "group": "upset", "name": "angry", "color": "tomato","category":"cat1", "score": -1, "icon": "e17" },
+  { "group": "upset", "name": "jealous", "color": "tomato","category":"cat1", "score": -1, "icon": "e11" },
+  { "group": "upset", "name": "frustrated", "color": "tomato","category":"cat1", "score": -1, "icon": "e17" },
+  { "group": "upset", "name": "confused", "color": "purple","category":"cat1", "score": -1, "icon": "e25" },
+  { "group": "upset", "name": "stressed", "color": "purple","category":"cat1", "score": -1, "icon": "e19" },
+  { "group": "upset", "name": "tired", "color": "purple","category":"cat1", "score": -1, "icon": "e08" },
+  { "group": "upset", "name": "overwhelmed", "color": "purple","category":"cat1", "score": -1, "icon": "e19" },
 ];  

efe6af13f2e5f7daf4f82c5684d699d9212c122e
Child-Of-Nyx <Child-Of-Nyx@users.noreply.github.com>
2021-11-29 14:09:52 -0600

    Minor refactoring for readability

--- emotion_checklist.js
+++ emotion_checklist.js
@@ -14,10 +14,13 @@
   // create html for each color
   html += "<div class='container d-flex flex-wrap' id='emotion-checklist-container'>";
   for (cat in emotionsByCat){
+
     html += "<span class=row row-cols-7>";
-    //html += "<h4 class='emotion-checklist-header text-center mb-1'>" + emotionsByCat[cat][0].category + "</h4>";
+
     for (emotion of emotionsByCat[cat]){
+      
       let checkId = `emotion-check-${emotion.name.split(" ").join("-")}`;
+      
       html += `<div class="col me-1" id='emotion-checklist-item'>
         <input type="checkbox" id=${checkId} name=${checkId} class="btn-check">
         <label  style="background-color:${emotion.color};"for="${checkId}" class="my-4 btn btn-primary-outline btn-square">

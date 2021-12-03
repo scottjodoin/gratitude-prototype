c625c744f6fd0a1191d2237671696a0a36008c9a
Robaraw <geletaroba@yahoo.ca>
2021-12-03 12:41:29 -0600

    updated menu bar

--- navbar.js
+++ navbar.js
@@ -2,19 +2,19 @@ $("header").html(
   `<nav class="navbar navbar-expand-md bg-dark">
   <div class="container">
     <a class="navbar-brand" href="index.html">LOGO</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+    <button class="navbar-menu-button navbar-dark navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
-        <li class="nav-item">
+        <li class="nav-item menu-separation">
           <a class="nav-link btn btn-primary text-white" href="checkin.html">Check in</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item menu-separation">
             <a class="nav-link btn btn-primary text-white" id=nav-view-entries href="view-entries.html">My progress</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item menu-separation">
             <a class="nav-link btn btn-primary text-white" id=nav-view-entries href="./goal-setting.html?view=goals-activities">Set Goals</a>
         </li>
       </ul>

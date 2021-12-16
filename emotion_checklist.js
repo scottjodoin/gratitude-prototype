6c18221bee54dccac2ac675f1959c2520927f45b
Camila B <Lcamila.bustos@yahoo.com>
2021-12-15 21:36:39 -0600

    Rearranged emotions

--- emotion_checklist.js
+++ emotion_checklist.js
@@ -15,14 +15,17 @@
   html += "<div class='container' id='emotion-checklist-container'>";
   html += "<div class='row row-cols-2'>";
 
-  console.log(emotionsByCat);
   for (cat in emotionsByCat) {
     html += "<div class='col'>";
-    // html += "<div class='d-flex flex-wrap mb-5'>";
-    for (emotion of emotionsByCat[cat]) {
+    for (let i = 0; i < emotionsByCat[cat].length - 2; i += 3) {
+      var emotion = emotionsByCat[cat][i];
+      var emotion2 = emotionsByCat[cat][i + 1];
+      var emotion3 = emotionsByCat[cat][i + 2];
+
       html += "<div class='row'><div class='col'>";
 
       let checkId = `emotion-check-${emotion.name.split(" ").join("-")}`;
+
       html += `<div class="me-4" id='emotion-checklist-item'>
         <input type="checkbox" id=${checkId} name=${checkId} class="btn-check">
         <label  style="background-color:${emotion.color};"for="${checkId}" class="my-3 btn btn-primary-outline btn-square">
@@ -31,18 +34,28 @@
         </label>
       </div>`;
 
-      html += "</div><div class='col'>";
-      html += `<div class="me-4" id='emotion-checklist-item'>
+      checkId = `emotion-check-${emotion2.name.split(" ").join("-")}`;
+
+      html += `</div><div class='col'><div class="me-4" id='emotion-checklist-item'>
         <input type="checkbox" id=${checkId} name=${checkId} class="btn-check">
-        <label  style="background-color:${emotion.color};"for="${checkId}" class="my-3 btn btn-primary-outline btn-square">
-          <img src="./img/${emotion.icon}.svg" width=40 height=40>
-          <span class="text-center">${emotion.name}</span>
+        <label  style="background-color:${emotion2.color};"for="${checkId}" class="my-3 btn btn-primary-outline btn-square">
+          <img src="./img/${emotion2.icon}.svg" width=40 height=40>
+          <span class="text-center">${emotion2.name}</span>
+        </label>
+      </div>`;
+
+      checkId = `emotion-check-${emotion3.name.split(" ").join("-")}`;
+
+      html += `</div><div class='col'><div class="me-4" id='emotion-checklist-item'>
+        <input type="checkbox" id=${checkId} name=${checkId} class="btn-check">
+        <label  style="background-color:${emotion3.color};"for="${checkId}" class="my-3 btn btn-primary-outline btn-square">
+          <img src="./img/${emotion3.icon}.svg" width=40 height=40>
+          <span class="text-center">${emotion3.name}</span>
         </label>
       </div>`;
 
       html += "</div></div>";
     }
-    // html += "</div>";
     html += "</div>";
   }
   html += "</div>";

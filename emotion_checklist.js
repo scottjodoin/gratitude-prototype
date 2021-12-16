94bcbb5f4b0dd6eec2703194b96f0d9d0ca4f924
Camila B <Lcamila.bustos@yahoo.com>
2021-12-15 21:21:01 -0600

    WIP - emotion layout

--- emotion_checklist.js
+++ emotion_checklist.js
@@ -12,13 +12,17 @@
   }
 
   // create html for each color
-  html +=
-    "<div class='container' id='emotion-checklist-container'>";
-  for (cat in emotionsByCat) {
-    html += "<div class='d-flex flex-wrap mb-5'>";
-    for (emotion of emotionsByCat[cat]) {
-      let checkId = `emotion-check-${emotion.name.split(" ").join("-")}`;
+  html += "<div class='container' id='emotion-checklist-container'>";
+  html += "<div class='row row-cols-2'>";
 
+  console.log(emotionsByCat);
+  for (cat in emotionsByCat) {
+    html += "<div class='col'>";
+    // html += "<div class='d-flex flex-wrap mb-5'>";
+    for (emotion of emotionsByCat[cat]) {
+      html += "<div class='row'><div class='col'>";
+
+      let checkId = `emotion-check-${emotion.name.split(" ").join("-")}`;
       html += `<div class="me-4" id='emotion-checklist-item'>
         <input type="checkbox" id=${checkId} name=${checkId} class="btn-check">
         <label  style="background-color:${emotion.color};"for="${checkId}" class="my-3 btn btn-primary-outline btn-square">
@@ -26,9 +30,22 @@
           <span class="text-center">${emotion.name}</span>
         </label>
       </div>`;
+
+      html += "</div><div class='col'>";
+      html += `<div class="me-4" id='emotion-checklist-item'>
+        <input type="checkbox" id=${checkId} name=${checkId} class="btn-check">
+        <label  style="background-color:${emotion.color};"for="${checkId}" class="my-3 btn btn-primary-outline btn-square">
+          <img src="./img/${emotion.icon}.svg" width=40 height=40>
+          <span class="text-center">${emotion.name}</span>
+        </label>
+      </div>`;
+
+      html += "</div></div>";
     }
+    // html += "</div>";
     html += "</div>";
   }
+  html += "</div>";
   html += "</div>";
   $("emotion-checklist").html(html);
 })();

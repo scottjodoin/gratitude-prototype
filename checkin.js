d103be8ce2654349aa18232d3da61671d0886920
Scott Jodoin <scottjodoin@gmail.com>
2021-12-16 11:55:07 -0600

    make progress bar change on no activities

---
+++ checkin.js
@@ -0,0 +1,18 @@
+let jsonstore = _storage.getItem("CheckInActiStorage");
+let activityCount = 0;
+if (jsonstore != null){
+  activityCount = JSON.parse(jsonstore)
+    .EachActivity
+    .filter(a=>a.exit)
+    .length;
+}
+
+if (activityCount === 0) {
+  // subtract 2 from count and total of progress because of the two activity-related screens
+  let map = {2:1,3:2,5:3};
+   $(".progress-tracker")
+    .each((i,e)=>{
+      let [count, total] = $(e).attr("progress").split("/");
+      $(e).attr("progress", `${map[count] || 0}/${total-2}`);
+    });
+}
